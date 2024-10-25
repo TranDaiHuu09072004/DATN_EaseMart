4e0160a4ffe2a4212c618b7a9d69d1223c4e8d6d
LeDucAnh2111 <lducanh2111@gmail.com>
2024-10-26 00:32:25 +0700

    update product pages

--- service/product.js
+++ service/product.js
@@ -5,3 +5,9 @@ export const getProByCate = async (category_id, idcate) => {
   const respone = await axios.get(`${url}/products?${category_id}=${idcate}`);
   return respone.data;
 };
+
+
+export const getProBy2Cate = async (category1,category2) => {
+  const respone = await axios.get(`${url}/products?${category1.name}=${category1.id}&${category2.name}=${category2.id}`);
+  return respone.data;
+};
