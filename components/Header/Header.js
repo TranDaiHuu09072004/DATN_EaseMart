5f7e7124e0f59fc233d06576171ba2da1c82129f
huu <trandaihuu4766@gmail.com>
2024-11-20 11:54:47 +0700

    test

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -31,7 +31,7 @@ export default function Header() {
     setCount(newCount);
   }, [state]);
 
-  useEffect(() => {
+  useEffect(() => {``
     const name = localStorage.getItem("name");
 
     if (name) {
